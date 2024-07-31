a6bd9cf3ecd940c8ff812ac28312e57642c021cf
Anastasia Savrukhina <assavr0@gmail.com>
2024-07-31 17:49:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return [...n.toString()]
+        .filter((elem) => elem.match(/\d/))
+        .reverse()
+        .join("");
+};
